00243727e41bfd4a4885f470b98752b9bc8b13fb
Duncan Smith <duncan@redgreencode.com>
2017-02-11 18:18:15 -0800

    Replace default image assets with real ones

--- TimeTortoise.UWP/Properties/AssemblyInfo.cs
+++ TimeTortoise.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeTortoise.UWP")]
+[assembly: AssemblyTitle("TimeTortoise")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TimeTortoise.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("TimeTortoise")]
+[assembly: AssemblyCopyright("Copyright © 2017 RedGreenCode")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
